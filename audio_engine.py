903b1e31d238770f9e541b4b6667c82ef96c9577
Dixit106 <dixitk2958@gmail.com>
2026-03-30 17:31:17 +0530

    learning to generate sin wave sound using multiple library and sin function

--- audio_engine.py
+++ audio_engine.py
@@ -1,12 +1,41 @@
 #trying to generate sin wave
 #importing part
-import numpy as np
+import pygame 
+import numpy 
 import math 
-import matplotlib.pyplot as plt
 
+pygame.init()
 
-time = np.arange(0,2*math.pi,math.pi/10)
+bits = 16
+sample_rate = 44100
+pygame.mixer.pre_init(sample_rate, bits)
 
-amplitude = np.sin(time)
-plt.plot(time,amplitude)
-plt.show()
+#formula thing for sin wave
+def sin_x(amp, freq, time):
+    return int(round(amp * math.sin(2 * math.pi * freq * time)))
+
+#tone class
+class Tone:
+    def sine(freq, duration=1, speaker=None):
+
+        num_samples = int(round(duration * sample_rate))
+
+        sound_buffer = numpy.zeros((num_samples, 2), dtype = numpy.int16)
+        amplitude = 2 ** (bits - 1) - 1
+
+        for sample in range(num_samples):
+            time = float(sample_num) / sample_rate
+
+            #generating x cordinate
+            sine = sine_x(amplitude, freq, time)
+
+            if speaker == 'r':
+                sound_buffer[sample_num][1] = sine 
+            if speaker == 'l':
+                sound_buffer[sample_num][0] = sine 
+            else:
+                sound_buffer[sample_num][1] = sine 
+                sound_buffer[sample_num][0] = sine        
+
+        sound = pygame.sndarray.make_sound(sound_buffer)
+        sound.play(loops=1, maxtime=int(duration * 100))        
