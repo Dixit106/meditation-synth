72c6dec302b7320a8eee772514ee556fc9139943
Dixit106 <dixitk2958@gmail.com>
2026-03-30 19:21:49 +0530

    main.py logic to start application but there are bugs

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+#importing stuff
+import pygame 
+import sys 
 from audio_engine import Tone 
 
+#keyboard button press option
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Synth Test Window")
 
-Tone.sine(440, duration=2)
-Tone.square(440)
+print("Press 1 for Sine, 2 for Square, 3 for Noise. Close window to quit.")
+
+running = True 
+while running:
+    for event in pygame.event.get():
+        #will let window close properly
+        if event.type == pygame.QUIT:
+            running = False 
+
+        #This to see which key is pressed
+            if event.key == pygame.K_1:
+                print("Playing Sine...")
+                Tone.sine(440, duration=1)
+            elif event.key == pygame.K_2:
+                print("Playing Square...")
+                Tone.square(440, duration=1)        
+
+pygame.quit()
+sys.exit()
