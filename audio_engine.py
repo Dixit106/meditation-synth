bbc7aeb45effaf5847845c077be0267a3155d3a3
Dixit106 <dixitk2958@gmail.com>
2026-03-30 18:24:27 +0530

    some changes in code (optimization) although its not working right now

--- audio_engine.py
+++ audio_engine.py
@@ -5,19 +5,37 @@ import numpy
 import math 
 import time
 
+#audio quality
 bits = 16
 sample_rate = 44100
 pygame.mixer.pre_init(sample_rate, -bits, 2)
 pygame.init()
 
-#formula thing for sin wave
-def sin_x(amp, freq, time):
-    return int(round(amp * math.sin(2 * math.pi * freq * time)))
 
 #tone class
 class Tone:
+
     @staticmethod 
     def sine(freq, duration=1, speaker=None):
+        #max volume for 16 bit audio
+        amplitude = 2 ** (bits - 1) - 1
+
+        #to generate time array using numpy
+        t = numpy.linspace(0, duration, int(sample_rate * duration), False)
+
+        # formula thing for sine wave
+        wave = amplitude * numpy.sin(2 * numpy.pi * freq * t)
+
+        #making stereo buffer
+        sound_buffer = numpy.zeros((len(wave), 2), dtype=numpy.int16)
+
+        #speaker logic
+        if speaker == 'r':
+            sound_buffer[:, 1] = wave # for right
+        elif speaker == 'l':
+            sound_buffer[:, 0] = wave # for left
+        else:
+            sound_buffer[:, 1] = wave # for both        
 
         num_samples = int(round(duration * sample_rate))
 
