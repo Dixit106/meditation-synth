fa314fa807656e88a55b7850611c21da2f597b47
Dixit106 <dixitk2958@gmail.com>
2026-03-31 21:04:09 +0530

    Learned and made demo application window with PyQt6

--- main.py
+++ main.py
@@ -14,4 +14,25 @@ class MeditationApp(QMainWindow):
 
         #CSS for the background of suitable vibe
         self.setStyleSheet("background-color: #1E1E1E; color: #FFFFFF;")
-        
+
+        #Creating a basic layout
+        layout = QVBoxLayout()
+
+        #Add a test title
+        title = QLabel("Welcome to the UI")
+        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        title.setStyleSheet("font-size: 24px; font-weight:bold; margin-top: 20px;")
+        layout.addWidget(title)
+
+        #setting layout at the center of the window
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MeditationApp()
+    window.show()
+
+    #To keep app running until click X to close it 
+    sys.exit(app.exec())                            
